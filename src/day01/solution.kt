3b5a77af74dcd05870ba3f4ac3dac589997954c6
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-01 17:56:16 +0000

    Tidy

---
+++ src/day01/solution.kt
@@ -0,0 +1,37 @@
+package day01
+
+import utils.Pos
+import kotlin.coroutines.experimental.buildSequence
+
+class Solution : utils.Solution() {
+    private fun walk() = buildSequence {
+        var current = Pos(0, 0)
+        var dir = Pos.D
+        inputText().split(", ").forEach { s ->
+            val n = s.substring(1).toInt()
+            dir = if (s[0] == 'R') dir.clockwise else dir.anticlockwise
+            for (i in 1..n) {
+                current += dir
+                yield(current)
+            }
+        }
+    }
+
+    override fun answer1(): String =
+        walk().last().size.toString()
+
+    override fun answer2(): String {
+        var s = HashSet<Pos>()
+        for (pos in walk()) {
+            if (s.contains(pos)) {
+                return pos.size.toString()
+            }
+            s.add(pos)
+        }
+        return ""
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution().run()
+}
---
+++ src/utils/Pos.kt
@@ -0,0 +1,36 @@
+package utils
+
+data class PosRange(override val start: Pos, override val endInclusive: Pos): ClosedRange<Pos>, Iterable<Pos> {
+    override fun iterator(): Iterator<Pos> =
+        (start.y..endInclusive.y).map { y ->
+            (start.x..endInclusive.x).map { x -> Pos(x, y) }
+        }.flatten().iterator()
+}
+
+data class Pos(val x: Int, val y: Int): Comparable<Pos> {
+    override fun compareTo(other: Pos) =
+        if (this == other) 0
+        else if (x > other.x || x == other.x && y > other.y) 1
+        else -1
+
+    operator fun plus(a: Pos) = Pos(x + a.x, y + a.y)
+
+    operator fun minus(a: Pos) = Pos(x - a.x, y - a.y)
+
+    operator fun times(n: Int) = Pos(x*n, y*n)
+
+    operator fun rangeTo(a: Pos) = PosRange(this, a-Pos(1, 1))
+
+    val size: Int
+        get() = Math.abs(x) + Math.abs(y)
+
+    val clockwise get() = Pos(y, -x)
+    val anticlockwise get() = Pos(-y, x)
+
+    companion object {
+        val U = Pos(0, -1)
+        val D = Pos(0, 1)
+        val L = Pos(-1, 0)
+        val R = Pos(1, 0)
+    }
+}
