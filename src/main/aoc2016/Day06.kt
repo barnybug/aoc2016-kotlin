fc117b95876d6f8fcc9346258f2bc41b22298dd8
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-02 22:23:41 +0000

    Day 6

--- src/main/aoc2016/Day06.kt
+++ src/main/aoc2016/Day06.kt
@@ -1,6 +1,22 @@
 package aoc2016
 
 class Day06(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    private val lines = input.split("\n")
+    private val length = lines[0].length
+
+    fun answer1() = (0 until length).map { i ->
+        lines.map { line -> line[i] }
+                .groupBy { it }
+                .map { it.key to it.value.size }
+                .sortedByDescending { it.second }
+                .first().first
+    }.joinToString("")
+
+    fun answer2() =(0 until length).map { i ->
+        lines.map { line -> line[i] }
+                .groupBy { it }
+                .map { it.key to it.value.size }
+                .sortedBy { it.second }
+                .first().first
+    }.joinToString("")
 }
--- src/test/aoc2016/Day06Test.kt
+++ src/test/aoc2016/Day06Test.kt
@@ -4,23 +4,25 @@ import org.junit.Assert
 import org.junit.Test
 
 internal class Day06Test {
+    private val testinput = "eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar"
+
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day06("").answer1())
+        Assert.assertEquals("easter", Day06(testinput).answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day06(Inputs.readText(6)).answer1())
+        Assert.assertEquals("qtbjqiuq", Day06(Inputs.readText(6)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day06("").answer2())
+        Assert.assertEquals("advent", Day06(testinput).answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day06(Inputs.readText(6)).answer2())
+        Assert.assertEquals("akothqli", Day06(Inputs.readText(6)).answer2())
     }
 }
