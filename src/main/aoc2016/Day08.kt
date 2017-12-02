c4ae7d6d30bbab1c45b482eaf323f6301b54ca56
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-02 22:04:58 +0000

    Template days

---
+++ src/main/aoc2016/Day08.kt
@@ -0,0 +1,6 @@
+package aoc2016
+
+class Day08(private val input: String) {
+    fun answer1() = input
+    fun answer2() = input
+}
---
+++ src/main/aoc2016/Day20.kt
@@ -0,0 +1,6 @@
+package aoc2016
+
+class Day20(private val input: String) {
+    fun answer1() = input
+    fun answer2() = input
+}
